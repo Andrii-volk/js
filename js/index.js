08c32b97da5be8ac32ed4b8e7a1ad7ed74f476fe
Andrii Boiarshynov <dzendokan.00@gmail.com>
2024-09-19 19:50:31 +0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// alert("hello world")
+console.log("hello wolrfd");
+
+let a = 1;
+
+console.log("a =", a);
+a = a + 1;
+console.log(a);
+
+const pi = 3.14;
+console.log(pi);
+
+//string
+
+const stringVariable1 = "string";
+const stringVariable2 = "string";
+const stringVariable3 = `strting`;
+console.log('Hey says : "Hy how are you? "');
+// литеральное задание значения
+
+//number
+// 1,22,-50 10,5 10e5,infinity,-infinity,0 (+0,-0),NaA-:"Not a number"
+
+const numberVariable1 = Infinity;
+const numberVariable2 = NaN;
+console.log('NaN :>> ', NaN);
+console.log(" numberVariable:>> ", numberVariable1);
+console.log(" 10/0", 10 / 0);
+console.log("10 / -0", 10 / -0);
+console.log('"abc" / 5', "abc" / 5)
