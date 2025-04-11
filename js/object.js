bf1bab882b3bcb0d5f72554ce88fc2fd6526dc18
Andrii Boiarshynov <dzendokan.00@gmail.com>
2025-04-11 21:00:32 +0300

    Add example function constructor object

--- js/object.js
+++ js/object.js
@@ -35,25 +35,74 @@
 // delete cat.color;
 // delete cat.age;
 
-const dog1 = {
-  name: 'bob',
-  age: 3,
-  color: 'black',
-  isMale: false,
-  say: function () {
-    return 'gav';
-  },
-};
+// const dog1 = {
+//   name: 'bob',
+//   age: 3,
+//   color: 'black',
 
-const dog2 = {
-  name: 'sven',
-  color: 'red',
-  age: 4,
-  say: function () {
-    return 'gav-gav';
-  },
-};
+//   say: function () {
+//     return 'gav';
+//   },
+// };
 
+// const dog2 = {
+//   name: 'sven',
+//   color: 'red',
+//   age: 4,
+//   say: function (voice) {
+//     return voice;
+//   },
+// };
+
+// console.log(dog1);
+// // dog1.age = 33;
+// console.log(dog1.age);
+
+// console.log(dog1.name, dog1.say('woof'));
+// console.log(dog1.say('gavvvvvvv'));
+
+function Dog(name, color, isMale) { 
+  this.name = name;
+  this.color = color;
+  this.isMAle = isMale;
+  this.say = function (voice) {
+    return voice
+    
+  }
+}
+
+const dog1 = new Dog( 'green', true);
+/* при вызове на место this становиться имя dog1
+dog1.name = 'Oscar';
+  dog1.color = "green";
+
+
+*/
 console.log(dog1);
-// dog1.age = 33;
-console.log(dog1.age);
+console.log(dog1.say('kdkdkdkddk'));
+const dog2 = new Dog('Bob', 'red', true);
+console.log(dog2);
+
+function ForUser(name, sname, age) {
+  this.name = name;
+  this.sname = sname;
+  this.age = age;
+  this.getFullNmae = function () {
+    return name + sname;
+    
+  }
+  
+}
+
+const foruser = new ForUser('andtii ', 'boyar', 33)
+
+console.log(foruser, foruser.getFullNmae());
+
+const foruser1 = new ForUser('bob ', 'bobovich', 3);
+
+console.log(foruser1, foruser1.getFullNmae());
+
+const foruser2 = new ForUser('sten ', 'test', 55);
+
+console.log(foruser2, foruser2.getFullNmae());
+
