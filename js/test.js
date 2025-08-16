b57b219d2c8de4f53dc6e20f691f8a1de581ae7b
Andrii Boiarshynov <dzendokan.00@gmail.com>
2025-08-16 20:25:14 +0300

    Eexample of linking objects via __proto__

--- js/test.js
+++ js/test.js
@@ -1,2 +1,30 @@
-'use strict'
+'use strict';
+const toString = function () {
+  return `${this.name} ${this.sname}`;
+};
 
+const man = {
+  read: function () {
+    return "i' am reading";
+  },
+};
+const student = {
+  id: 1,
+  name: 'ellon',
+  sname: 'musk',
+  age: 50,
+  toString: toString,
+};
+const student2 = {
+  id: 2,
+  name: 'bob',
+  sname: 'mrofbusk',
+  age: 30,
+  toString, // toString:toString
+};
+// student.prototype =man
+student.__proto__ = man;
+
+console.log(student.read());
+console.log(student2);
+console.log(student.toString);
