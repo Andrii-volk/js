bd4260774c299d7980e996854a30c964066e173f
Andrii Boiarshynov <dzendokan.00@gmail.com>
2025-08-17 20:33:26 +0300

    Example .prototype

--- js/test.js
+++ js/test.js
@@ -29,32 +29,5 @@ console.log(student.read());
 console.log(student2);
 console.log(student.toString);
 //!------------------------------------------------------------------------------------
-const rabbitParent = {
-  countPaw: 4,
-  jump: function () {
-    return 'i am jumping';
-  },
-};
-
-const rabbitPet = {
-  name: 'Jorg',
-  pet: function () {
-    return 'pet-' + this.name;
-  },
-};
-
-const rabbitMagic = {
-  say: function (word) {
-    return word;
-  },
-};
-rabbitPet.__proto__ = rabbitParent;
-rabbitMagic.__proto__ = rabbitPet;
-
-// console.log(rabbitParent.say('hello'));
-// console.log(rabbitPet.pet());
-console.log(rabbitMagic.jump());
-console.log(rabbitMagic.say('hello'));
-
-
- 
+//
+//!==================================================================================
