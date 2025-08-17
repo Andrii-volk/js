25e4c7a84b7c5c255817341098c7545f08ac7f4b
Andrii Boiarshynov <dzendokan.00@gmail.com>
2025-08-17 21:20:02 +0300

    Example prototype.v1.1

--- js/test.js
+++ js/test.js
@@ -31,3 +31,40 @@ console.log(student.toString);
 //!------------------------------------------------------------------------------------
 //
 //!==================================================================================
+
+function StudentPrototypePrototype() {
+  this.eat = function () {
+    return 'i am eat';
+  };
+}
+function StudentPrototype() {
+  this.speak = function () {
+    return `${this.name} ${this.sname}щщщщщщщщщщщщщщщщщщщщщщ`;
+  };
+}
+StudentPrototype.prototype = new StudentPrototypePrototype();
+function Student(name, sname, age = 22) {
+  this.name = name;
+  this.sname = sname;
+  this.age = age;
+  this.fuulName = function () {
+    return `${this.name}-${this.sname}-${this.age}`;
+  };
+}
+
+Student.prototype = new StudentPrototype();
+
+const studentMain = new Student('musk', 'dedede', 44);
+
+console.log(studentMain.eat());
+// console.log(studentMain.sleep());
+console.log(studentMain.fuulName());
+console.log(studentMain.speak());
+const studentMain1 = new Student('mgfgfsk', 'fg', 5555);
+console.log(studentMain1.speak());
+console.log(studentMain1.fuulName());
+console.log(studentMain1.fuulName());
+
+
+
+
